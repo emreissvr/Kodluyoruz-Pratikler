6f789bcdfd6b777917bbba53dd23f076343970d2
emreissvr <emreiissever@gmail.com>
2021-09-20 03:32:41 +0300

    mesaj

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            byte a = 5; // 1 byte
+            sbyte b = 5; // 1 byte
+
+            short  c = 5;  // 2 byte
+            ushort d = 5;  // 2 byte  (positive number)
+
+            Int16 e = 2;    // 2 byte
+            int f = 2;  // 4 byte
+            Int32  i32= 4; //  4 byte
+            Int64 i_64= 5; // 8 byte
+            uint  ui = 5;  // 4 byte (positive)
+            long ul = 5; // 8 byte
+            
+            float fl = 4;   // 4 byte
+            double db = 6;  // 8 byte
+            decimal de = 5 ; // 16 byte
+
+            char ch = '2';  // 2 byte
+            string str = "Zikriye"; // sınırsız
+            bool bl = true;
+            bool bo = false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = "y";
+            object o3 = 4;
+            object o4 = 4.3;
+            // object üst türdür hepsini alabilir aslnda değişkenler burdan türemiştir
+
+            // string ifadeleri 
+            string str1 = string.Empty;
+            str1 = "Zikriye Ürkmez";
+            string ad = "zikriye";
+            string soyad = "ürkmez";
+            string tamisim = ad + " " + soyad;
+
+            // integer tanımlama şekilleri 
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer2 * integer1;
+
+            //boolean
+            bool bool1 = 10<2;
+
+            // değişken dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+
+            string yenideger = str20 + int20.ToString();
+            Console.WriteLine(yenideger);
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            System.Console.WriteLine(int21); // çıktı 40
+
+            int int22 = int20 + int.Parse(str20);
+            */
+            
+            // int degisken = 5;
+            // string degisken = "emre";
+            // System.Console.WriteLine(degisken);
+            // short degisken = 32,767;
+            // uint degisken2 = -4;
+        }
+    }
+}
