ae76a3d893ac9958335b803d808423cb71a31526
emreissvr <emreiissever@gmail.com>
2021-10-02 03:47:56 +0300

    main10

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhileAndForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.Write("Lütfen bir sayı giriniz:");
+        //    int sayi = int.Parse(Console.ReadLine());
+        //    int sayac = 1;
+        //    int toplam = 0;
+        //    while(sayac <= sayi)
+        //    {
+        //        toplam += sayac;
+        //        sayac ++;
+        //    }
+        //     Console.WriteLine(toplam/sayi);
+
+        //     // a dan z ye kadar tüm karakterleri yaz
+        //     char character = 'a';
+        //     while(character < 'z')
+        //     {
+        //         Console.Write(character);
+        //         character++;
+        //     }
+            System.Console.WriteLine("****** foreach ******");
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                System.Console.WriteLine(araba);
+            }
+        }
+    }
+}
