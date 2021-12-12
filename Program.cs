6e11cabbc0c8f166b24ba7e8f31873da50c90649
emreissvr <emreiissever@gmail.com>
2021-12-13 02:19:37 +0300

    main20

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Employee employee1 = new Employee();
+            employee1.Name = "emre";
+            employee1.Surname = "issever";
+            employee1.No = 21312312;
+            employee1.Department = "Information Technology";
+
+            employee1.EmployeeInformations();
+
+            System.Console.WriteLine("*****************************");
+
+            Employee employee2 = new Employee();
+            employee2.Name = "deniz";
+            employee2.Surname = "arda";
+            employee2.No = 123213;
+            employee2.Department = "purchasing department";
+
+            employee2.EmployeeInformations();
+
+
+
+        }
+    }
+
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public int No;
+        public string Department;
+
+        public void EmployeeInformations(){
+            System.Console.WriteLine("Employee Name : {0}",Name);
+            System.Console.WriteLine("Employee Surname : {0}",Surname);
+            System.Console.WriteLine("Employee No : {0}",No);
+            System.Console.WriteLine("Employee Department : {0}",Department);
+
+
+        }
+    }
+}
