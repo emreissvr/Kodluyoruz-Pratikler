564a3932885ca3fc4a66e2c0babbbb5c7d06f8b8
emreissvr <emreiissever@gmail.com>
2021-12-15 21:28:25 +0300

    main24

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 32;
+
+            if(sıcaklık <= (int)HavaDurumu.Normal){
+                System.Console.WriteLine("dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            }
+            else if(sıcaklık  >= (int)HavaDurumu.Sıcak){
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık <(int)HavaDurumu.CokSıcak ){
+                System.Console.WriteLine("hadi dışarıya çıkalım.");
+            }
+
+        }
+    }
+
+
+    enum Gunler
+    {
+        Pazartesi,
+        Sali ,
+        Carsamba,
+        Persembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu{
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
